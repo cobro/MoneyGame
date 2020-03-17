55d834572255207e1b504a501bf6a915780c36c4
Janek <amok.skull@gmail.com>
2020-03-17 12:09:37 +0100

    Scriptable Object

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public new string name;
+    public string problemDescription;
+    public string answerA;
+    public string answerB;
+    public int phycisalA = 0;
+    public int physicalB = 0;
+    public int mentalA = 0;
+    public int mentalB = 0;
+    public int moneyA = 0;
+    public int moneyB = 0;
+    public int familyA = 0;
+    public int familyB = 0;
+    public int friendsA = 0;
+    public int friendsB = 0;
+}
