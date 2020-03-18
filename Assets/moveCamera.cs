62894f9344a802d2dc4c6b4ed16006d54c53b3c8
Janek <amok.skull@gmail.com>
2020-03-18 13:31:31 +0100

    wazza

--- Assets/moveCamera.cs
+++ Assets/moveCamera.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class moveCamera : MonoBehaviour
 {
-    public Camera mainCam;
-    Vector3 tempCameraPos;
+    public CinemachineDollyCart vCam;
     public static bool cameraAnimating = false;
+    public static int NextPathPosition;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
 
     public IEnumerator animateCamera()
     {
         cameraAnimating = true;
         yield return new WaitForSeconds(.5f);
-        tempCameraPos = mainCam.transform.position;
-        while (mainCam.transform.position.x < tempCameraPos.x +850)
+        startTime = Time.time;
+        journeyLength = vCam.m_Position;
+        /*while (mainCam.transform.position.x < tempCameraPos.x +850)
         {
             mainCam.transform.position = Vector3.Lerp(mainCam.transform.position, new Vector3(tempCameraPos.x + 850, tempCameraPos.y, tempCameraPos.z), 0.1f * Time.deltaTime);
             Debug.Log("Lerping");
@@ -22,6 +27,6 @@ public class moveCamera : MonoBehaviour
                 mainCam.transform.position = new Vector3(tempCameraPos.x + 850, transform.position.y, transform.position.z);
                 cameraAnimating = false;
             }
-        }
+        }*/
     }
 }
