68d9d55e993566ded5be04f8d41f6a96b43da8cb
Janek <amok.skull@gmail.com>
2020-03-20 13:21:44 +0100

    Fixed that mf

--- Assets/Scripts/SummaryCard.cs
+++ Assets/Scripts/SummaryCard.cs
@@ -13,14 +13,15 @@ public class SummaryCard : MonoBehaviour
 
     public void lastContinueBeforeSummary()
     {
-        health.text = "Health: " + CardHolder.summaryHealth.ToString();
+        health.text = "Health: " + ((float)System.Math.Round(CardHolder.summaryHealth, 1)).ToString();
         if (Mathf.Abs(CardHolder.summarySocial) < .05)
             social.text = " Social 0";
         else
-            social.text = "Social: " + CardHolder.summarySocial.ToString();
+            social.text = "Social: " + ((float)System.Math.Round(CardHolder.summarySocial, 1)).ToString();
         
         if (CardHolder.tookALoan == true)
         {
+            
             moneyTracker.Money -= 664;
             currentBalance.text = moneyTracker.Money.ToString();
             money.text = "Money: " + CardHolder.summaryMoney.ToString() + " - 664 (Loan interest) = " + (CardHolder.summaryMoney - 664).ToString();
